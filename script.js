de0188bf16339ae084a07c2922e59903e9950396
Shohrux <topilovshohrux696@gmail.com>
2023-04-13 10:06:32 +0300

    version__1

---
+++ script.js
@@ -0,0 +1,9 @@
+confirm ('ZAFFA EDU OQUV MARKAZIGA XUSH KELIBSIZ ! ');
+var ism = prompt ('Ismingizni kiriting!');
+confirm ('Salom ' + ism);
+var yosh = prompt ('Yoshingiz nechida ?');
+var kurs = prompt ('Qaysi kursga yozdirmoqchisiz?');
+var tel = prompt ('Telefon raqamingiz kiriting');
+confirm('Sizning sorovnomangiz qabul qilindi , biz siz bilan boglanamiz.');
+confirm('Etiboringiz uchun raxmat! Royxatdan otish yakunlandi.');
+console.log('Ismi: '+ ism, ', Yoshi: '+ yosh, ', Kurs nomi: ' + kurs, ', Telefon raqami: ' + tel, ', Registratsiya yakunlandi' );
